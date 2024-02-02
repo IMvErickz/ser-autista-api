828938c3eabcfd58d0e256a8a94edae3d17dd30b
IMvErickz <erickspy2003@gmail.com>
2024-02-02 10:53:00 -0300

    refact: get excert news

--- api/routes/News/getAllNews.ts
+++ api/routes/News/getAllNews.ts
@@ -5,6 +5,15 @@ export async function GetAllNews(app: FastifyInstance) {
     app.get('/news', async () => {
         const news = await prisma.news.findMany()
 
-        return { news }
+        const response = news.map(n => {
+            return {
+                id: n.id,
+                title: n.title,
+                imgUrl: n.imgUrl,
+                excert: n.content.substring(0, 300).concat('...')
+            }
+        })
+
+        return { response }
     })
 }
