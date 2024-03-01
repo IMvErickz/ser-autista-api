c515e5b364a313eff354697a335cf8089d53216d
IMvErickz <erickspy2003@gmail.com>
2024-03-01 16:41:44 -0300

    fix: conditions to update professional

--- api/routes/Professionals/updateProfessional.ts
+++ api/routes/Professionals/updateProfessional.ts
@@ -21,19 +21,77 @@ export async function UpdateProfessional(app: FastifyInstance) {
 
         const { id } = professionalId.parse(req.params)
 
-        await prisma.professional.update({
-            where: {
-                id: id
-            },
-            data: {
-                name,
-                number,
-                address,
-                email,
-                specialty,
-                description
-            }
-        })
+        if (name) {
+            await prisma.professional.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    name,
+                }
+            })
+        } else if (number) {
+            await prisma.professional.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    number
+                }
+            })
+        } else if (address) {
+            await prisma.professional.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    address
+                }
+            })
+        } else if (email) {
+            await prisma.professional.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    email
+                }
+            })
+        } else if (specialty) {
+            await prisma.professional.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    specialty
+                }
+            })
+        } else if (description) {
+            await prisma.professional.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    description
+                }
+            })
+        } else {
+            await prisma.professional.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    name,
+                    number,
+                    address,
+                    email,
+                    specialty,
+                    description
+                }
+            })
+        }
+
+        
 
         return res.send(204).send()
 
